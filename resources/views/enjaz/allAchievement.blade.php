f23409bee9824256ac376906dd239d766598cf4d
Ghada-Alkurdi <ghada.a.k94@gmail.com>
2023-03-02 13:44:17 +0200

    view all articles , initiatives , achievement and awards ghada 2-3-3023

---
+++ resources/views/enjaz/allAchievement.blade.php
@@ -0,0 +1,46 @@
+<div class="container articles my-5 pt-3">
+    <div class="row section-title my-4">
+        <h1 class="h3 mb-0 text-gray-800">{{__('enjaz.achievements')}}</h1>
+    </div>
+    @if($contents->count() > 0)
+        <div class="row">
+            @foreach($contents as $content)
+                <div class="col-12 col-lg-3 col-md-4 col-sm-12 mb-3">
+                    <a href="{{route('index.details.achievement',['name_en'=>\Illuminate\Support\Str::slug($content->user->name_en),'id'=>$content->id])}}">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-header">
+                                {{$content->title}}
+                            </div>
+                            <img src="{{url($content->content_file->first()->AttPath)}}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">
+                                <div class="info-1 d-flex justify-content-between">
+                                    <div class="user-name card-text-a">
+                                        <i class="fas fa-user"></i>
+                                        {{$content->user->name_ar}}
+                                    </div>
+                                    <div class="statics-info card-text-a">
+                                        <i class="fas fa-eye ms-2"></i>{{$content->views}}
+                                        <i class="fas fa-comment ms-2"></i>10
+                                        <i class="fas fa-heart ms-2"></i>{{$content->likes}}
+                                    </div>
+                                </div>
+                                <div class="info-2 d-flex justify-content-between">
+                                    <div class="user-name card-text-a">
+                                        <i class="fas fa-book-open"></i>
+                                        {{$content->classification->name}}
+                                    </div>
+                                    <div class="statics-info card-text-a">
+                                        <i class="fas fa-calendar-week ms-2"></i>
+                                        {{$content->created_at->format('Y/m/d')}}
+                                    </div>
+                                </div>
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            @endforeach
+        </div>
+    @endif
+</div>
